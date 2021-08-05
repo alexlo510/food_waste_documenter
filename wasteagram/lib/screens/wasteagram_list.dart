423360774fb788e9619f57e02d0052ae151d06e9
Alex Lo <alexlo493@gmail.com>
2021-08-04 20:07:05 -0700

    extracted the stream builder list into a function

--- wasteagram/lib/screens/wasteagram_list.dart
+++ wasteagram/lib/screens/wasteagram_list.dart
@@ -1,6 +1,7 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-//import 'package:firebase_storage/firebase_storage.dart';
+import 'package:wasteagram/models/food_waste_post.dart';
 
 class WasteagramList extends StatefulWidget {
 
@@ -24,17 +25,9 @@ class _WasteagramListState extends State<WasteagramList> {
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       body: StreamBuilder(
         stream: FirebaseFirestore.instance.collection('posts').snapshots(),
-        builder: (content, AsyncSnapshot snapshot) {
+        builder: (context, AsyncSnapshot snapshot) {
           if (snapshot.hasData && snapshot.data.docs != null && snapshot.data.docs.length > 0) {
-            return ListView.builder(
-              itemCount: snapshot.data.docs.length,
-              itemBuilder: (context, index) {
-                var post = snapshot.data.docs[index];
-                return ListTile(
-                  title: Text(post['date']),
-                );
-              }
-            );
+            return FoodWastePostsList(context, snapshot);
           } else {
             return Center(child: CircularProgressIndicator());
           }
@@ -42,4 +35,21 @@ class _WasteagramListState extends State<WasteagramList> {
       ),
     );
   }
+
+  Widget FoodWastePostsList(BuildContext context, AsyncSnapshot snapshot){
+    return ListView.builder(
+      itemCount: snapshot.data.docs.length,
+      itemBuilder: (context, index) {
+        FoodWastePost post = FoodWastePost.fromJSON(snapshot.data.docs[index].data());
+        return ListTile(
+          title: Text('${post.date}'),
+          trailing: Text(
+            '${post.quantity}',
+            style: Theme.of(context).textTheme.headline4,
+          ),
+        );
+      }
+    );
+  }
+
 }
