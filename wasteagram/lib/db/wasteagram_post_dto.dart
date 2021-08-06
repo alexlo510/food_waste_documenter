8f4c2b47e2d77ffe690cbfbbf89766e74f325f08
Alex Lo <alexlo493@gmail.com>
2021-08-05 17:16:05 -0700

    created dto

---
+++ wasteagram/lib/db/wasteagram_post_dto.dart
@@ -0,0 +1,19 @@
+class WasteagramPostDTO{
+  String? date;
+  String? imageURL;
+  int? quantity;
+  double? latitude;
+  double? longitude;
+
+  WasteagramPostDTO({
+    this.date,
+    this.imageURL,
+    this.quantity,
+    this.latitude,
+    this.longitude,
+  });
+
+  String toString() {
+    return 'Date: $date, ImageURL: $imageURL, Quality: $quantity, Latitude: $latitude, Longitude: $longitude';
+  }
+}
