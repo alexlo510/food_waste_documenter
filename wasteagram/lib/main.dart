4ca42684094fe03b254f452a9f35270faa35424d
Alex Lo <alexlo493@gmail.com>
2021-08-05 18:16:19 -0700

    commented out landscape orientations

--- wasteagram/lib/main.dart
+++ wasteagram/lib/main.dart
@@ -6,8 +6,8 @@ import 'package:wasteagram/app.dart';
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await SystemChrome.setPreferredOrientations([
-    DeviceOrientation.landscapeLeft,
-    DeviceOrientation.landscapeRight,
+    //DeviceOrientation.landscapeLeft,
+    //DeviceOrientation.landscapeRight,
     DeviceOrientation.portraitUp,
   ]);
   await Firebase.initializeApp();
