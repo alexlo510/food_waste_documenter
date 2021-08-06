80c85d94d82111def2058189fe151e4660835c7c
Alex Lo <alexlo493@gmail.com>
2021-08-05 17:16:13 -0700

    created form field and save button

--- wasteagram/lib/screens/new_entry_screen.dart
+++ wasteagram/lib/screens/new_entry_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:wasteagram/db/wasteagram_post_dto.dart';
 
 class NewEntryScreen extends StatefulWidget {
 
@@ -16,6 +17,10 @@ class NewEntryScreen extends StatefulWidget {
 }
 
 class _NewEntryScreenState extends State<NewEntryScreen> {
+
+  final formKey = GlobalKey<FormState>();
+  final wasteagramPostDTO = WasteagramPostDTO();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,10 +40,59 @@ class _NewEntryScreenState extends State<NewEntryScreen> {
                 child: Image.file(widget.image)
               ),
             ),
-            
+            newEntryForm(),
+            saveButton(context: context, formKey: formKey, wasteagramPostDTO: wasteagramPostDTO),
           ],
         ),
       ),
     );
   }
+
+  Widget newEntryForm(){
+    return Form(
+      key: formKey,
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            numberOfItemsFormField(wasteagramPostDTO: wasteagramPostDTO),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget numberOfItemsFormField({required WasteagramPostDTO wasteagramPostDTO}){
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 10),
+      child: TextFormField(
+        autofocus: true,
+        decoration: InputDecoration(
+            labelText: 'Number Of Items',
+            border: OutlineInputBorder(),
+        ),
+        onSaved: (value) {
+          wasteagramPostDTO.quantity = int.tryParse(value as String);
+        },
+        validator: (value) => (value!.isEmpty || int.tryParse(value) == null) ? 'Please enter an integer' :  null,
+      ),
+    );
+  }
+
+  Widget saveButton({required BuildContext context, required dynamic formKey, required WasteagramPostDTO wasteagramPostDTO}) {
+    return ElevatedButton(
+      onPressed: () async {
+        if (formKey.currentState.validate()){
+          formKey.currentState.save();
+          // do database work here
+        }
+      }, 
+      child: Text('Save'),
+      style: ElevatedButton.styleFrom(
+        primary: Colors.grey[300],
+        onPrimary: Colors.black,
+      )
+    );
+  }
+  
 }
